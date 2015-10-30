b124ed83e8ffb11cc6e94255918eb4c413cf4581
Amir Ahmadi <uk_amir@hotmail.co.uk>
2015-10-30 20:52:56 +0000

    added openshift properties

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
 });
@@ -25,6 +29,6 @@ io.on('connection', function(socket) {
    });
 });
 
-http.listen(3000, function() {
-   console.log('Listening on 3000');
+http.listen(server_port, server_ip_address, function () {
+   console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
