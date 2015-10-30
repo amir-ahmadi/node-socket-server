65a8b43e334fc397431582e686a36617d2017f96
Amir Ahmadi <uk_amir@hotmail.co.uk>
2015-10-30 20:08:50 +0000

    Minor

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-});
-
 io.on('connection', function(socket) {
    console.log('A user connected');
    socket.on('disconnect', function() {
